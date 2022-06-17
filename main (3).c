e08d3e2531fcd65061ef7e3881554c32557bcaba
sathish1313 <104289461+sathish1313@users.noreply.github.com>
2022-06-17 22:49:42 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+int find_nth_term(int n, int a, int b, int c) {
+if(n==1){
+return a;
+}
+if(n==2){
+return b;
+}
+if(n==3){
+return c;
+}
+else{
+return find_nth_term(n-1,a,b,c)+find_nth_term(n-2,a,b,c)+find_nth_term(n-3,a,b,c);
+}
+}
+int main() {
+int n, a, b, c;
+scanf("%d %d %d %d", &n, &a, &b, &c);
+int ans = find_nth_term(n, a, b, c);
+printf("%d", ans);
+return 0;
+}
+
